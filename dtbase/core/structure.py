b0d174f91fd698a3d32ca0c70645f663f30efb3a
nbarlowATI <nbarlow@turing.ac.uk>
2023-02-24 15:57:16 +0000

    first implementation of location-related tables, and associated tests.

---
+++ dtbase/core/structure.py
@@ -0,0 +1,158 @@
+"""
+Module to define the structure of the database. Each Class, defines a table in the database.
+    __tablename__: creates the table with the name given
+    __table_args__: table arguments eg: __table_args__ = {'sqlite_autoincrement': True}
+    BASE: the declarative_base() callable returns a new base class from which all mapped classes
+    should inherit. When the class definition is completed, a new Table and mapper() is generated.
+"""
+import enum
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Float,
+    ForeignKey,
+    Integer,
+    JSON,
+    String,
+    Text,
+    UniqueConstraint,
+)
+from sqlalchemy.dialects.postgresql import UUID
+
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from sqlalchemy.sql.expression import false
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+from bcrypt import gensalt, hashpw
+
+SQLA = SQLAlchemy()
+BASE = SQLA.Model
+
+
+class LocationClass(BASE):
+    """
+    This class describes all the physical locations in the farm.
+    """
+
+    __tablename__ = "location"
+
+    # columns
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    # relationshionships (One-To-Many)
+    string_values_relationship = relationship("LocationStringValueClass")
+    integer_values_relationship = relationship("LocationIntegerValueClass")
+    float_values_relationship = relationship("LocationFloatValueClass")
+
+    # arguments
+    __table_args__ = (UniqueConstraint("id"),)
+
+
+class LocationStringIdentifierClass(BASE):
+    """
+    Any string variables that can be used to identify locations in the farm.
+    """
+    __tablename__ = "location_string_variable"
+
+    # columns
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    units = Column(String(100), nullable=True)
+    __table_args__ = (UniqueConstraint("name","units"),)
+
+
+class LocationIntegerIdentifierClass(BASE):
+    """
+    Any integer variables that can be used to identify locations in the farm.
+    """
+    __tablename__ = "location_integer_variable"
+
+    # columns
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    units = Column(String(100), nullable=True)
+    __table_args__ = (UniqueConstraint("name","units"),)
+
+
+class LocationFloatIdentifierClass(BASE):
+    """
+    Any floating point number variables that can be used to identify locations in the farm.
+    """
+    __tablename__ = "location_float_variable"
+
+    # columns
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    units = Column(String(100), nullable=True)
+    __table_args__ = (UniqueConstraint("name","units"),)
+
+
+class LocationStringValueClass(BASE):
+    """
+    The value of a string variable that can be used to identify locations in the farm.
+    """
+    __tablename__ = "location_string_value"
+
+
+    # columns
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    value = Column(String(100), nullable=False)
+    variable_id = Column(
+        Integer,
+        ForeignKey("location_string_variable.id"),
+        nullable=False,
+    )
+    location_id = Column(
+        Integer,
+        ForeignKey("location.id"),
+        nullable=False,
+    )
+    __table_args__ = (UniqueConstraint("variable_id","location_id"),)
+
+
+class LocationIntegerValueClass(BASE):
+    """
+    The value of an integer variable that can be used to identify locations in the farm.
+    """
+    __tablename__ = "location_integer_value"
+
+    # columns
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    value = Column(Integer, nullable=False)
+    variable_id = Column(
+        Integer,
+        ForeignKey("location_integer_variable.id"),
+        nullable=False,
+    )
+    location_id = Column(
+        Integer,
+        ForeignKey("location.id"),
+        nullable=False,
+    )
+    __table_args__ = (UniqueConstraint("variable_id","location_id"),)
+
+
+class LocationFloatValueClass(BASE):
+    """
+    The value of a floating point number variable that can be used to identify locations in the farm.
+    """
+    __tablename__ = "location_float_value"
+
+    # columns
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    value = Column(Float, nullable=False)
+    variable_id = Column(
+        Integer,
+        ForeignKey("location_float_variable.id"),
+        nullable=False,
+    )
+    location_id = Column(
+        Integer,
+        ForeignKey("location.id"),
+        nullable=False,
+    )
+    __table_args__ = (UniqueConstraint("variable_id","location_id"),)
---
+++ dtbase/tests/test_db.py
@@ -0,0 +1,61 @@
+"""
+Test the basic structure of the database, creating rows and relations via
+the SQLAlchemy ORM.
+"""
+
+from dtbase.core.structure import (
+    LocationClass,
+    LocationStringIdentifierClass,
+    LocationIntegerIdentifierClass,
+    LocationFloatIdentifierClass,
+    LocationStringValueClass,
+    LocationIntegerValueClass,
+    LocationFloatValueClass
+)
+
+
+def test_add_location_empty(session):
+    loc = LocationClass()
+    session.add(loc)
+    session.commit()
+    assert isinstance(loc.id, int)
+    session.close()
+
+
+def test_add_location_identifiers(session):
+    si = LocationStringIdentifierClass(name="zone")
+    ii = LocationIntegerIdentifierClass(name="aisle")
+    fi = LocationFloatIdentifierClass(name="distance", units="m")
+    session.add(si)
+    session.add(ii)
+    session.add(fi)
+    session.commit()
+    assert isinstance(si.id, int)
+    assert isinstance(ii.id, int)
+    assert isinstance(fi.id, int)
+    session.close()
+
+
+def test_add_xyz_location(session):
+    loc = LocationClass()
+    x = LocationFloatIdentifierClass(name="x", units="m")
+    y = LocationFloatIdentifierClass(name="y", units="m")
+    z = LocationFloatIdentifierClass(name="z", units="m")
+    session.add(loc)
+    session.add(x)
+    session.add(y)
+    session.add(z)
+    session.commit()
+    xval = LocationFloatValueClass(value=2.0, variable_id=x.id, location_id=loc.id)
+    yval = LocationFloatValueClass(value=3.0, variable_id=y.id, location_id=loc.id)
+    zval = LocationFloatValueClass(value=4.0, variable_id=z.id, location_id=loc.id)
+    session.add(xval)
+    session.add(yval)
+    session.add(zval)
+    session.commit()
+    # see if we can retrieve the x,y,z values from the location
+    coords = loc.float_values_relationship
+    assert coords[0].value == 2.0
+    assert coords[1].value == 3.0
+    assert coords[2].value == 4.0
+    session.close()
