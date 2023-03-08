a81de016ae743770516ddf1e4ff1c07c755d8130
nbarlowATI <nbarlow@turing.ac.uk>
2023-03-08 19:35:16 +0000

    merge develop

---
+++ dtbase/backend/dtbase_app.py
@@ -0,0 +1,16 @@
+from os import environ
+from sys import exit
+
+from flask_migrate import Migrate
+from config import config_dict
+from dtbase.backend.api import create_app, db
+
+get_config_mode = environ.get("DTBASE_CONFIG_MODE", "Production")
+
+try:
+    config_mode = config_dict[get_config_mode.capitalize()]
+except KeyError:
+    exit("Error: Invalid DTBASE_CONFIG_MODE environment variable entry.")
+
+app = create_app(config_mode)
+Migrate(app, db)
