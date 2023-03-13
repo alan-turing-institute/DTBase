d67d63069866d90f44ba09f9729bd386f6af0ad9
nbarlowATI <nbarlow@turing.ac.uk>
2023-03-13 17:12:08 +0000

    fix import of config

--- dtbase/backend/dtbase_app.py
+++ dtbase/backend/dtbase_app.py
@@ -2,7 +2,7 @@ from os import environ
 from sys import exit
 
 from flask_migrate import Migrate
-from config import config_dict
+from dtbase.backend.config import config_dict
 from dtbase.backend.api import create_app, db
 
 get_config_mode = environ.get("DTBASE_CONFIG_MODE", "Production")
