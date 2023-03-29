3aeabd5280a1ab3e792207a9cf6e41655994ba38
mastoffel <martin.adam.stoffel@gmail.com>
2023-03-29 15:57:17 +0100

    Format code

--- dtbase/backend/utils.py
+++ dtbase/backend/utils.py
@@ -15,20 +15,22 @@ def add_default_session(func):
 
 def check_keys(payload, keys, api_endpoint):
     """Check if `keys` are in `payload` and return a json response if not.
-    
+
     Args:
         payload: Dictionary to check.
         keys: List required keys to check for.
         api_endpoint: API endpoint that was called.
-    
+
     Returns:
         None if all keys are in payload, otherwise a json response with an error.
     """
-    
+
     missing = [k for k in keys if k not in payload.keys()]
     if missing:
         return (
-            jsonify({"error": f"Must include {missing} in POST request to {api_endpoint}."}),
+            jsonify(
+                {"error": f"Must include {missing} in POST request to {api_endpoint}."}
+            ),
             400,
         )
     return None
