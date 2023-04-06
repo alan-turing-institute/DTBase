1b93ea6329bf79c55286466780e4594cb98f978e
nbarlowATI <nbarlow@turing.ac.uk>
2023-04-06 14:18:30 +0100

    enable running with local docker container with postgres

--- dtbase/tests/conftest.py
+++ dtbase/tests/conftest.py
@@ -1,7 +1,4 @@
 import os
-import re
-import subprocess
-import sys
 import time
 
 import pytest
@@ -17,6 +14,11 @@ from dtbase.core.db import (
     drop_tables,
     session_close,
     session_open,
+)
+from dtbase.core.db_docker import (
+    check_for_docker,
+    start_docker_postgres,
+    stop_docker_postgres
 )
 from dtbase.core.utils import create_user
 
@@ -66,77 +68,21 @@ def runner(app):
     return app.test_cli_runner()
 
 
-def check_for_docker():
-    """
-    See if we have a postgres docker container already running.
-
-    Returns
-    =======
-    container_id:str if container running,
-    OR
-    True if docker is running, but no postgres container
-    OR
-    False if docker is not running
-    """
-    p = subprocess.run(["docker", "ps"], capture_output=True)
-    if p.returncode != 0:
-        return False
-    output = p.stdout.decode("utf-8")
-    m = re.search("([0-9a-f]+)[\s]+postgres", output)
-    if not m:
-        return True  # Docker is running, but no postgres container
-    else:
-        return m.groups()[0]  # return the container ID
-
-
 def pytest_configure(config):
     """
     Allows plugins and conftest files to perform initial configuration.
     This hook is called for every plugin and initial conftest
     file after command line options have been parsed.
     """
-    # see if docker is running, and if so, if postgres container exists
-    docker_info = check_for_docker()
-    if not docker_info:  # docker desktop not running at all
-        DOCKER_RUNNING = False
-        print("Docker not found - will skip tests that use the database.")
-        return
-    DOCKER_RUNNING = True
-    if isinstance(docker_info, bool):
-        # docker is running, but no postgres container
-        print("Starting postgres docker container")
-        p = subprocess.run(
-            [
-                "docker",
-                "run",
-                "-e",
-                "POSTGRES_DB=dtdb",
-                "-e",
-                "POSTGRES_USER=postgres",
-                "-e",
-                "POSTGRES_PASSWORD=postgres",
-                "-d",
-                "-p",
-                "5432:5432",
-                "postgres:11",
-            ],
-            capture_output=True,
-        )
-        if p.returncode != 0:
-            print("Problem starting Docker container - is Docker running?")
-            return
-        else:
-            # wait a while for the container to start up
-            time.sleep(10)
-            # save the docker container id so we can stop it later
-            global DOCKER_CONTAINER_ID
-            DOCKER_CONTAINER_ID = p.stdout.decode("utf-8")
-            print(f"Setting DOCKER_CONTAINER_ID to {DOCKER_CONTAINER_ID}")
 
     # move on with the rest of the setup
     print(
         "pytest_configure: start " + SQL_TEST_CONNECTION_STRING + " " + SQL_TEST_DBNAME
     )
+    global DOCKER_CONTAINER_ID
+    DOCKER_CONTAINER_ID = start_docker_postgres()
+    if DOCKER_CONTAINER_ID:
+        print(f"Setting DOCKER_CONTAINER_ID to {DOCKER_CONTAINER_ID}")
     # create database so that we have tables ready
     create_database(SQL_TEST_CONNECTION_STRING, SQL_TEST_DBNAME)
     time.sleep(1)
@@ -155,6 +101,5 @@ def pytest_unconfigure(config):
     assert success, log
     # if we started a docker container in pytest_configure, kill it here.
     if DOCKER_CONTAINER_ID:
-        print(f"Stopping docker container {DOCKER_CONTAINER_ID}")
-        os.system("docker kill " + DOCKER_CONTAINER_ID)
+        stop_docker_postgres(DOCKER_CONTAINER_ID)
     print("pytest_unconfigure: end")
