6a33e69b1c577a1c839c1f3e7a896b09be8f4722
mastoffel <martin.adam.stoffel@gmail.com>
2023-04-27 12:50:01 +0100

    Fix js to produce new identifiers correctly

--- dtbase/webapp/app/base/static/javascript/location_schema.js
+++ dtbase/webapp/app/base/static/javascript/location_schema.js
@@ -1,19 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const addButton = document.querySelector('.btn-add-identifier');
-    const identifierGroup = document.querySelector('.form-group:nth-of-type(2)');
+    const identifierGroup = document.querySelector('.form-group:nth-of-type(3)');
 
-    addButton.addEventListener('click', function() {
+    addButton.addEventListener('click', function () {
         const newRow = document.createElement('div');
         newRow.className = 'identifier-row';
 
         newRow.innerHTML = `
-            <input type="text" class="form-control" name="identifier_name[]" placeholder="Name" required>
-            <input type="text" class="form-control" name="identifier_units[]" placeholder="Units" required>
-            <input type="text" class="form-control" name="identifier_datatype[]" placeholder="Datatype" required>
+            <input type="text" class="form-control" name="identifier_name[]" placeholder="name, e.g. longitude" required>
+            <input type="text" class="form-control" name="identifier_units[]" placeholder="units, e.g. degrees" required>
+            <select class="form-control custom-select datatype-select" name="identifier_datatype[]" required>
+                <option disabled selected value="">-- Select datatype --</option>
+                <option value="string">string</option>
+                <option value="float">float</option>
+                <option value="integer">integer</option>
+                <option value="boolean">boolean</option>
+            </select>
             <button type="button" class="btn btn-danger btn-remove-identifier">-</button>
         `;
 
-        newRow.querySelector('.btn-remove-identifier').addEventListener('click', function() {
+        newRow.querySelector('.btn-remove-identifier').addEventListener('click', function () {
             newRow.remove();
         });
 
