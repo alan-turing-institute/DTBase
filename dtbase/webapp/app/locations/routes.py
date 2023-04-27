8f6974065bc538dbc0d819440a618f8e090512be
mastoffel <martin.adam.stoffel@gmail.com>
2023-04-27 09:24:19 +0100

    Flash message when schema exists already

--- dtbase/webapp/app/locations/routes.py
+++ dtbase/webapp/app/locations/routes.py
@@ -10,52 +10,66 @@ from app.locations import blueprint
 import utils
 from flask import redirect, url_for, flash
 
-@blueprint.route("/index", methods=["GET", "POST"])
+@blueprint.route("/index", methods=["GET"])
 @login_required
 def index():
-  
-    if request.method == "POST":
-        name = request.form.get("name")
-        description = request.form.get("description")
-        identifier_names = request.form.getlist("identifier_name[]")
-        identifier_units = request.form.getlist("identifier_units[]")
-        identifier_datatypes = request.form.getlist("identifier_datatype[]")
+    return render_template("location_schema_form.html")
 
-        identifiers = [
-            {
-                "name": identifier_name,
-                "units": identifier_unit,
-                "datatype": identifier_datatype
-            }
-            for identifier_name, identifier_unit, identifier_datatype in zip(
-                identifier_names, identifier_units, identifier_datatypes
-            )
-        ]
+@blueprint.route("/index", methods=["POST"])
+@login_required
+def submit_location_schema():
+    name = request.form.get("name")
+    description = request.form.get("description")
+    identifier_names = request.form.getlist("identifier_name[]")
+    identifier_units = request.form.getlist("identifier_units[]")
+    identifier_datatypes = request.form.getlist("identifier_datatype[]")
 
-        payload = {
-            "name": name,
-            "description": description,
-            "identifiers": identifiers
+    identifiers = [
+        {
+            "name": identifier_name,
+            "units": identifier_unit,
+            "datatype": identifier_datatype
         }
+        for identifier_name, identifier_unit, identifier_datatype in zip(
+            identifier_names, identifier_units, identifier_datatypes
+        )
+    ]
 
-        idnames = []
-        for identifier in payload["identifiers"]:
-            idnames.append(identifier["name"])
+    payload = {
+        "name": name,
+        "description": description,
+        "identifiers": identifiers
+    }
 
-        idnames.sort()
+    # idnames = []
+    # for identifier in payload["identifiers"]:
+    #     idnames.append(identifier["name"])
 
-     # Send a POST request to the new backend route
-        data = {
-                "name": payload["name"],
-                "description": payload["description"],
-                "identifiers": identifiers,
-            }
+    # idnames.sort()
 
+    data = {
+        "name": payload["name"],
+        "description": payload["description"],
+        "identifiers": identifiers,
+    }
+
+    existing_schemas_response = utils.backend_call("get", "/location/list_location_schemas")
+    existing_schemas = existing_schemas_response.json()
+    
+    # check if the schema already exists
+    if any(schema['name'] == name for schema in existing_schemas):
+        flash(f"The schema '{name}' already exists.", "error")
+        return redirect(url_for(".index"))
+    
+    try:
         response = utils.backend_call("post", "/location/insert_location_schema", data)
-        
-        if response.status_code != 201:
-                # TODO Write a more useful reaction to this.
-                raise RuntimeError(f"A backend call failed: {response}")
+    except Exception as e:
+        flash(f"Error communicating with the backend: {e}", "error")
+        return redirect(url_for(".index"))
+
+    if response.status_code != 201:
+        flash(f"An error occurred while adding the location schema: {response}", "error")
+    else:
         response_data = response.json()
 
         if response_data.get("status") == "success":
@@ -63,4 +77,4 @@ def index():
         else:
             flash("Failed to add location schema", "error")
 
-    return render_template("location_schema_form.html")
+    return redirect(url_for(".index"))
