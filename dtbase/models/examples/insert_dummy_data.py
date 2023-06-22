af8f3808deaf7bf3673e04e77911892f87fc8001
nbarlowATI <nbarlow@turing.ac.uk>
2023-06-22 14:01:29 +0100

    move examples directory to models/

--- dtbase/models/examples/insert_dummy_data.py
+++ dtbase/models/examples/insert_dummy_data.py
@@ -1,10 +1,11 @@
-'''
+"""
 Sample code to insert two sensors (each with two measures) together with
 corresponding dummy measurements
-'''
+"""
 
 from dtbase.core.utils import *
 from dtbase.tests.upload_synthetic_data import *
+
 session = get_db_session()
 insert_trh_readings(session, sensor_unique_id="TRH1")
 insert_trh_readings(session, sensor_unique_id="TRH2", measure_suffix="_s2")
--- dtbase/models/examples/run_hodmd.py
+++ dtbase/models/examples/run_hodmd.py
@@ -3,11 +3,10 @@ from datetime import datetime
 from dtbase.core.utils import *
 from dtbase.models.hodmd.run_pipeline import *
 
-plots_save_path = './plots/hodmd/' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+plots_save_path = "./plots/hodmd/" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 if not os.path.exists(plots_save_path):
     os.makedirs(plots_save_path)
 
 session = get_db_session()
-#run_pipeline(session, plots_save_path, multi_measure=False)
+# run_pipeline(session, plots_save_path, multi_measure=False)
 run_pipeline(session, plots_save_path, multi_measure=True)
-
