0ad83a8e28edc8eba371553fe1646da955c7c039
nbarlowATI <nbarlow@turing.ac.uk>
2023-06-22 14:41:27 +0100

    update examples for HODMD model

--- dtbase/models/examples/insert_dummy_data.py
+++ dtbase/models/examples/insert_dummy_data.py
@@ -3,9 +3,9 @@ Sample code to insert two sensors (each with two measures) together with
 corresponding dummy measurements
 """
 
-from dtbase.core.utils import *
-from dtbase.tests.upload_synthetic_data import *
+from dtbase.core.utils import get_db_session
+from dtbase.tests.upload_synthetic_data import insert_trh_readings
 
 session = get_db_session()
 insert_trh_readings(session, sensor_unique_id="TRH1")
-insert_trh_readings(session, sensor_unique_id="TRH2", measure_suffix="_s2")
+insert_trh_readings(session, sensor_unique_id="TRH2")
--- dtbase/tests/upload_synthetic_data.py
+++ dtbase/tests/upload_synthetic_data.py
@@ -19,44 +19,58 @@ from dtbase.tests.generate_synthetic_data import (
 )
 
 
-def insert_trh_sensor(sensor_unique_id, session, suffix):
+def insert_trh_sensor(sensor_unique_id, session):
     """
     Insert a temperature / relative humidity sensor.
     """
-    insert_sensor_measure(
-        name="Temperature" + suffix,
-        units="Degrees C",
-        datatype="float",
-        session=session,
-    )
-    insert_sensor_measure(
-        name="Humidity" + suffix, units="Percent", datatype="float", session=session
-    )
-    insert_sensor_type(
-        name=sensor_unique_id,
-        description="Temperature/Humidity",
-        measures=[
-            {"name": "Temperature", "units": "Degrees C", "datatype": "float"},
-            {"name": "Humidity", "units": "Percent", "datatype": "float"},
-        ],
-        session=session,
-    )
-    insert_sensor(
-        type_name=sensor_unique_id, unique_identifier=sensor_unique_id, session=session
-    )
+    try:
+        insert_sensor_measure(
+            name="Temperature",
+            units="Degrees C",
+            datatype="float",
+            session=session,
+        )
+    except:
+        session.rollback()
+    try:
+        insert_sensor_measure(
+            name="Humidity", units="Percent", datatype="float", session=session
+        )
+    except:
+        session.rollback()
+    try:
+        insert_sensor_type(
+            name=sensor_unique_id,
+            description="Temperature/Humidity",
+            measures=[
+                {"name": "Temperature", "units": "Degrees C", "datatype": "float"},
+                {"name": "Humidity", "units": "Percent", "datatype": "float"},
+            ],
+            session=session,
+        )
+    except:
+        session.rollback()
+    try:
+        insert_sensor(
+            type_name=sensor_unique_id,
+            unique_identifier=sensor_unique_id,
+            session=session,
+        )
+    except:
+        session.rollback()
 
 
-def insert_trh_readings(
-    session, sensor_unique_id="TRH1", measure_suffix=None, insert_sensor=True
-):
+def insert_trh_readings(session, sensor_unique_id="TRH1", insert_sensor=True):
     """
     Insert a set of temperature and humidity readings for a sensor
     with unique_identifier 'TRH1'
     """
-    if measure_suffix is None:
-        measure_suffix = ""
     if insert_sensor:
-        insert_trh_sensor(sensor_unique_id, session, measure_suffix)
+        try:
+            insert_trh_sensor(sensor_unique_id, session)
+        except:
+            print("Sensor already inserted")
+            session.rollback()
     # generate the Temperature and Humidity readings
     readings = generate_trh_readings(sensor_ids=[1])
     # readings will be a pandas dataframe.
@@ -66,14 +80,14 @@ def insert_trh_readings(
     temps = list(readings.temperature)
     humids = list(readings.humidity)
     insert_sensor_readings(
-        measure_name="Temperature" + measure_suffix,
+        measure_name="Temperature",
         sensor_uniq_id=sensor_unique_id,
         readings=temps,
         timestamps=timestamps,
         session=session,
     )
     insert_sensor_readings(
-        measure_name="Humidity" + measure_suffix,
+        measure_name="Humidity",
         sensor_uniq_id=sensor_unique_id,
         readings=humids,
         timestamps=timestamps,
