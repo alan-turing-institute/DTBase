c42cbbe8a14a50a30f0aea31eb72abb82886fcbe
Markus Hauru <mhauru@turing.ac.uk>
2023-11-24 10:28:00 +0000

    Use dict.get in constants.py

--- dtbase/core/constants.py
+++ dtbase/core/constants.py
@@ -35,12 +35,10 @@ def make_conn_string(
 
 # Create connection string
 SQL_ENGINE = "postgresql"
-SQL_USER = os.environ["DT_SQL_USER"].strip() if "DT_SQL_USER" in os.environ else "DUMMY"
-SQL_PASSWORD = (
-    os.environ["DT_SQL_PASS"].strip() if "DT_SQL_PASS" in os.environ else "DUMMY"
-)
-SQL_HOST = os.environ["DT_SQL_HOST"].strip() if "DT_SQL_HOST" in os.environ else "DUMMY"
-SQL_PORT = os.environ["DT_SQL_PORT"].strip() if "DT_SQL_PORT" in os.environ else "DUMMY"
+SQL_USER = os.environ.get("DT_SQL_USER", "DUMMY").strip()
+SQL_PASSWORD = os.environ.get("DT_SQL_PASS", "DUMMY").strip()
+SQL_HOST = os.environ.get("DT_SQL_HOST", "DUMMY").strip()
+SQL_PORT = os.environ.get("DT_SQL_PORT", "DUMMY").strip()
 SQL_CONNECTION_STRING = make_conn_string(
     SQL_ENGINE,
     SQL_USER,
@@ -48,38 +46,16 @@ SQL_CONNECTION_STRING = make_conn_string(
     SQL_HOST,
     SQL_PORT,
 )
-SQL_DBNAME = (
-    os.environ["DT_SQL_DBNAME"].strip().lower()
-    if "DT_SQL_DBNAME" in os.environ
-    else "DUMMY"
-)
+SQL_DBNAME = os.environ.get("DT_SQL_DBNAME", "DUMMY").strip().lower()
 SQL_DEFAULT_DBNAME = "postgres"
 SQL_SSLMODE = "require"
 
 # same for the temporary db used for unit testing
-SQL_TEST_USER = (
-    os.environ["DT_SQL_TESTUSER"].strip()
-    if "DT_SQL_TESTUSER" in os.environ
-    else "DUMMY"
-)
-SQL_TEST_PASSWORD = (
-    os.environ["DT_SQL_TESTPASS"].strip()
-    if "DT_SQL_TESTPASS" in os.environ
-    else "DUMMY"
-)
-SQL_TEST_HOST = (
-    os.environ["DT_SQL_TESTHOST"].strip()
-    if "DT_SQL_TESTHOST" in os.environ
-    else "DUMMY"
-)
-SQL_TEST_PORT = (
-    os.environ["DT_SQL_TESTPORT"].strip()
-    if "DT_SQL_TESTPORT" in os.environ
-    else "DUMMY"
-)
-SQL_TEST_DBNAME = (
-    os.environ["DT_SQL_TESTDBNAME"] if "DT_SQL_TESTDBNAME" in os.environ else "test_db"
-)
+SQL_TEST_USER = os.environ.get("DT_SQL_TESTUSER", "DUMMY").strip()
+SQL_TEST_PASSWORD = os.environ.get("DT_SQL_TESTPASS", "DUMMY").strip()
+SQL_TEST_HOST = os.environ.get("DT_SQL_TESTHOST", "DUMMY").strip()
+SQL_TEST_PORT = os.environ.get("DT_SQL_TESTPORT", "DUMMY").strip()
+SQL_TEST_DBNAME = os.environ.get("DT_SQL_TESTDBNAME", "test_db")
 
 SQL_TEST_CONNECTION_STRING = make_conn_string(
     SQL_ENGINE,
@@ -90,11 +66,7 @@ SQL_TEST_CONNECTION_STRING = make_conn_string(
 )
 
 # backend API base URL, used by frontend, ingress, and model functions.
-CONST_BACKEND_URL = (
-    os.environ["DT_BACKEND_URL"]
-    if "DT_BACKEND_URL" in os.environ
-    else "http://localhost:5000"
-)
+CONST_BACKEND_URL = os.environ.get("DT_BACKEND_URL", "http://localhost:5000")
 
 # how many rows to display in tables
 CONST_MAX_RECORDS = 50000
@@ -102,25 +74,21 @@ CONST_MAX_RECORDS = 50000
 CONST_TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 DEFAULT_USER_EMAIL = "default_user@localhost"
-DEFAULT_USER_PASS = (
-    os.environ["DT_DEFAULT_USER_PASS"] if "DT_DEFAULT_USER_PASS" in os.environ else None
-)
+DEFAULT_USER_PASS = os.environ.get("DT_DEFAULT_USER_PASS", None)
 
 # The following are some examples of how one might store URLs and API keys
 # for data sources that we want to use for data ingress.
 
 # Weather-related misc constants
-CONST_LAT = os.environ["DT_LAT"] if "DT_LAT" in os.environ else "DUMMY"  #  latitude
-CONST_LON = os.environ["DT_LONG"] if "DT_LONG" in os.environ else "DUMMY"  #  longitude
+CONST_LAT = os.environ.get("DT_LAT", "DUMMY")  #  latitude
+CONST_LON = os.environ.get("DT_LONG", "DUMMY")  #  longitude
 
 # We use OpenWeatherMap as an example of how to get weather data
 # (both historical and forecast).
 
-CONST_OPENWEATHERMAP_APIKEY = (
-    os.environ["DT_OPENWEATHERMAP_APIKEY"].strip()
-    if "DT_OPENWEATHERMAP_APIKEY" in os.environ
-    else "DUMMY"
-)
+CONST_OPENWEATHERMAP_APIKEY = os.environ.get(
+    "DT_OPENWEATHERMAP_APIKEY", "DUMMY"
+).strip()
 
 # see https://openweathermap.org/api/one-call-3
 CONST_OPENWEATHERMAP_HISTORICAL_URL = (
