8fff6ddc8e49f9b79f4bd32294c5a0f08f197745
Edwin Brown <w.edwin.brown@outlook.com>
2023-12-04 11:11:37 +0000

    cleaning up weather ingress

--- dtbase/ingress/ingress_weather.py
+++ dtbase/ingress/ingress_weather.py
@@ -11,8 +11,6 @@ import requests
 from dtbase.core.constants import (
     CONST_OPENWEATHERMAP_FORECAST_URL,
     CONST_OPENWEATHERMAP_HISTORICAL_URL,
-    DEFAULT_USER_EMAIL,
-    DEFAULT_USER_PASS,
 )
 from dtbase.ingress.ingress_utils import (
     add_sensor_types,
@@ -92,6 +90,7 @@ SENSORS = [
 
 def query_openweathermap_api(
     dt_to: datetime,
+    **timedelta_kwargs: int,
 ) -> Tuple[bool, str, Optional[pd.DataFrame]]:
     """
     Retrieve weather data from the openweathermap API.
@@ -105,6 +104,10 @@ def query_openweathermap_api(
     Parameters
     ----------
     dt_to: datetime, latest time for returned records
+    days: int, number of days to retrieve data going back in time from dt_to.
+      Default is 2. Parameter is ignored if dt_to is in the future.
+    **timedelta_kwargs: keyword arguments to pass to timedelta,
+    e.g. hours=1, minutes=30, etc.
 
     Returns
     -------
@@ -112,22 +115,31 @@ def query_openweathermap_api(
     error: str, empty string if everything OK
     df: pd.DataFrame, contains 1 row per hours data.
     """
-    timestamps = []
+    # timestamps = []
     if dt_to <= datetime.now():
         base_url = CONST_OPENWEATHERMAP_HISTORICAL_URL
-        dt_from = dt_to - timedelta(days=2)
-        timestamps.append(int(dt_from.timestamp()))
+        dt_from = dt_to - timedelta(**timedelta_kwargs)
+        # timestamps.append(int(dt_from.timestamp()))
         logging.info(
             f"Calling Openweathermap historical API from {dt_from} to {dt_to}."
         )
     else:
         base_url = CONST_OPENWEATHERMAP_FORECAST_URL
-        logging.info(f"Calling Openweathermap forecast API to {dt_to}.")
+        dt_from = datetime.now()
+        logging.info(f"Calling Openweathermap forecast API from {dt_from} to {dt_to}.")
 
-    timestamps.append(int(dt_to.timestamp()))
+    timestamps = [
+        int(dt.timestamp())
+        for dt in list(pd.date_range(dt_from, dt_to, freq="d").to_pydatetime())
+    ]
+
+    print(timestamps)
+
+    # timestamps.append(int(dt_to.timestamp()))
     hourly_records = []
     error = ""
     # do API call and get data in right format for both dates.
+
     for ts in timestamps:
         url = base_url + "&dt={}".format(ts)
         response = requests.get(url)
@@ -156,6 +168,8 @@ def query_openweathermap_api(
             hourly_records.append(record)
     weather_df = pd.DataFrame(hourly_records)
     weather_df.set_index("timestamp", inplace=True)
+
+    weather_df.to_csv(f"Testing_{dt_from}-{dt_to}.csv")
     success = True
     log = "\nSuccess: Weather dataframe \n{}".format(weather_df)
     logging.info(log)
@@ -168,6 +182,7 @@ def import_openweathermap_data(
     backend_user: str,
     backend_password: str,
     create_sensors: bool = False,
+    **timedelta_kwargs: int,
 ) -> Tuple[bool, str]:
     """
     This is the main function for this module.
@@ -185,8 +200,15 @@ def import_openweathermap_data(
     Returns:
         success, error
     """
+
+    # Check that the sensor_uniq_id is in valid options defined in SENSORS
+    if sensor_uniq_id not in [sensor["unique_identifier"] for sensor in SENSORS]:
+        error = f"Invalid sensor_uniq_id: {sensor_uniq_id}"
+        logging.error(error)
+        return False, error
+
     access_token = backend_login(backend_user, backend_password)
-    success, error, df = query_openweathermap_api(dt_to)
+    success, error, df = query_openweathermap_api(dt_to, **timedelta_kwargs)
     if not success:
         logging.error(error)
         return success, error
@@ -195,8 +217,18 @@ def import_openweathermap_data(
     all_timestamps = [ts.isoformat() for ts in df.index]
 
     if create_sensors:
+
+        # Filter out the sensor we want to create based on sensor_uniq_id
+        add_sensors(
+            [
+                sensor
+                for sensor in SENSORS
+                if sensor["unique_identifier"] == sensor_uniq_id
+            ],
+            access_token=access_token,
+        )
+
         add_sensor_types(SENSOR_TYPES, access_token=access_token)
-        add_sensors(SENSORS, access_token=access_token)
 
     for metric in df.columns:
         values = df[metric]
@@ -224,12 +256,34 @@ def import_openweathermap_data(
 
 
 if __name__ == "__main__":
-    dt_to = datetime.now() + timedelta(hours=24)
-    sensor_uniq_id = "OpenWeatherMapForecast"
-    import_openweathermap_data(
-        dt_to,
-        sensor_uniq_id,
-        create_sensors=True,
-        backend_user=DEFAULT_USER_EMAIL,
-        backend_password=DEFAULT_USER_PASS,
-    )
+
+    # Set DT_LONG and DT_LAT environment variables to your location
+
+    # Test historical query 4 days from past
+    now = datetime.now()
+    success, error, df = query_openweathermap_api(dt_to=now, days=3)
+
+    # Test forecast query 4 days into future
+    future = datetime.now() + timedelta(days=4)
+    success, error, df = query_openweathermap_api(dt_to=future, days=3)
+
+    # # Example Weather Forecast usage:
+    # dt_to = datetime.now() + timedelta(days=2)
+    # sensor_uniq_id = "OpenWeatherMapForecast"
+    # import_openweathermap_data(
+    #     dt_to,
+    #     sensor_uniq_id,
+    #     create_sensors=True,
+    #     backend_user=DEFAULT_USER_EMAIL,
+    #     backend_password=DEFAULT_USER_PASS,
+    # )
+
+    # dt_to = datetime.now() - timedelta(hours=48)
+    # sensor_uniq_id = "OpenWeatherMapHistory"
+    # import_openweathermap_data(
+    #     dt_to,
+    #     sensor_uniq_id,
+    #     create_sensors=True,
+    #     backend_user=DEFAULT_USER_EMAIL,
+    #     backend_password=DEFAULT_USER_PASS,
+    # )
