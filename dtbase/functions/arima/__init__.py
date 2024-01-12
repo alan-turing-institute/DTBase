41ae72d836ed127b57f75b224b924f961638dc33
Markus Hauru <mhauru@turing.ac.uk>
2024-01-12 16:18:04 +0000

    Azure functions test container

---
+++ dtbase/functions/arima/__init__.py
@@ -0,0 +1,28 @@
+import logging
+
+from azure.functions import HttpRequest, HttpResponse
+
+
+def main(req: HttpRequest) -> HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    name = req.params.get("name")
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get("name")
+
+    if name:
+        return HttpResponse(
+            f"Hello, {name}. This HTTP triggered function executed successfully."
+        )
+    else:
+        return HttpResponse(
+            "This HTTP triggered function executed successfully. "
+            "Pass a name in the query string or in the request body for a personalized "
+            "response.",
+            status_code=200,
+        )
