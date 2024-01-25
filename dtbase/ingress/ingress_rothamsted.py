874252bab9c1c99bd3f0df7f68dc71fc6fb2437b
Markus Hauru <mhauru@turing.ac.uk>
2024-01-25 15:53:48 +0000

    Start using latest features of the Rothamsted API

--- dtbase/ingress/ingress_rothamsted.py
+++ dtbase/ingress/ingress_rothamsted.py
@@ -13,26 +13,6 @@ from dtbase.ingress.ingress_base import BaseIngress
 
 ROTHAMSTED_API_BASE_URL = "https://nwfp.rothamsted.ac.uk:8443"
 
-# We are only including these measurements for now, to keep the amount of data and the
-# number of queries manageable.
-INCLUDED_MEASUREMENTS = {
-    "Nitrite and Nitrate",
-    "pH",
-    "Precipitation",
-    "Soil Temperature @ 15cm Depth",
-    "Soil Moisture @ 10cm Depth",
-    "Soil Moisture @ 20cm Depth",
-    "Soil Moisture @ 30cm Depth",
-    "Air Temperature",
-    "Relative Humidity",
-    "Carbon Dioxide",
-    "Solar Radiation",
-}
-
-# Special values that the catchmentName field may have, but that aren't returned when
-# querying for all catchments.
-EXTRA_CATCHMENTS = [{"DisplayName": "Met"}]
-
 
 def query_rothamsted_api(
     method: str, endpoint_name: str, payload: Optional[dict | list] = None
@@ -57,97 +37,81 @@ class RothamstedIngress(BaseIngress):
     def get_data(
         self, from_dt: dt.datetime, to_dt: dt.datetime
     ) -> list[tuple[str, dict | list]]:
-        catchments = cast(list, query_rothamsted_api("get", "getCatchments"))
-        catchments += EXTRA_CATCHMENTS
-        measurement_types = query_rothamsted_api("get", "getMeasurementTypes")
+        measurement_types = cast(
+            list, query_rothamsted_api("get", "getMeasurementTypes")
+        )
+        measurement_types_by_id = {mt["Id"]: mt for mt in measurement_types}
+        catchment_measurement_types = cast(
+            list, query_rothamsted_api("get", "getCatchmentMeasurementTypes")
+        )
+
+        sensor_types_created = set()
+        sensors_created = set()
         return_value: list[tuple[str, dict | list]] = []
-        for mt in measurement_types:
-            sensor_type_created = False
-            measurement_type_name = mt["DisplayName"]
-            if measurement_type_name not in INCLUDED_MEASUREMENTS:
+        for cmt in catchment_measurement_types:
+            mt = measurement_types_by_id[cmt["type_id"]]
+            catchment_name = cmt["catchment_name"]
+            if catchment_name is None and cmt["location_Name"] == "Met":
+                # "Met" is considered a catchment by some API endpoints, and not by
+                # others, so this gets a bit confusing.
+                catchment_name = "Met"
+
+            readings = query_rothamsted_api(
+                "post",
+                "getMeasurementsByCatchmentName",
+                {
+                    "startDate": from_dt.isoformat(),
+                    "endDate": to_dt.isoformat(),
+                    "catchmentName": catchment_name,
+                    "typeId": cmt["type_id"],
+                },
+            )
+            readings = [r for r in readings if r["dataQuality"] == "Acceptable"]
+            if not readings:
                 continue
-            sensor_type_name = f"{measurement_type_name} Sensor"
-            for catchment in catchments:
-                catchment_name = catchment["DisplayName"]
-                readings = query_rothamsted_api(
-                    "post",
-                    "getMeasurementsByCatchmentName",
-                    {
-                        "startDate": from_dt.isoformat(),
-                        "endDate": to_dt.isoformat(),
-                        "catchmentName": catchment_name,
-                        "typeId": mt["Id"],
-                    },
-                )
-                readings = [r for r in readings if r["dataQuality"] == "Acceptable"]
-                if not readings:
-                    continue
 
-                if not sensor_type_created:
-                    # Only create the sensor type if we actually found data for it, and
-                    # it hasn't been created before.
-                    payload = {
-                        "name": sensor_type_name,
-                        "description": None,
-                        "measures": [
-                            {
-                                "name": measurement_type_name,
-                                "units": mt["Unit"],
-                                "datatype": "float",
-                            },
-                        ],
-                    }
-                    logging.info(f"Inserting sensor type {payload}")
-                    return_value.append(("/sensor/insert-sensor-type", payload))
-                    sensor_type_created = True
+            # Only create the sensor type if we actually found data for it, and it
+            # hasn't been created before.
+            sensor_type_name = f"{mt['DisplayName']} Sensor"
+            if sensor_type_name not in sensor_types_created:
+                payload = {
+                    "name": sensor_type_name,
+                    "description": None,
+                    "measures": [
+                        {
+                            "name": mt["DisplayName"],
+                            "units": mt["Unit"],
+                            "datatype": "float",
+                        },
+                    ],
+                }
+                logging.info(f"Inserting sensor type {payload}")
+                return_value.append(("/sensor/insert-sensor-type", payload))
+                sensor_types_created.add(sensor_type_name)
 
-                sensor_id = f"{catchment_name} {sensor_type_name}"
+            # Only create the sensor if we actually found data for it, and it hasn't
+            # been created before.
+            sensor_id = f"{catchment_name} {sensor_type_name}"
+            if sensor_id not in sensors_created:
                 payload = {
                     "type_name": sensor_type_name,
                     "unique_identifier": sensor_id,
                 }
                 logging.info(f"Inserting sensor {payload}")
                 return_value.append(("/sensor/insert-sensor", payload))
-                timestamps, values = zip(
-                    *[(r["DateTime"], float(r["Value"])) for r in readings]
-                )
-                payload = {
-                    "measure_name": measurement_type_name,
-                    "unique_identifier": sensor_id,
-                    "readings": values,
-                    "timestamps": timestamps,
-                }
-                return_value.append(("/sensor/insert-sensor-readings", payload))
-        return return_value
+                sensors_created.add(sensor_id)
 
-
-def gather_all_catchment_measurement_pairs() -> dict:
-    measurement_types = query_rothamsted_api("get", "getMeasurementTypes")
-    return_value = {}
-    for mt in measurement_types:
-        mt_id = mt["Id"]
-        readings = query_rothamsted_api(
-            "post",
-            "getMeasurementsByTypeId",
-            payload={"numPerPage": 500, "page": 1, "typeId": mt_id},
-        )
-        if not readings:
-            continue
-        location_names, catch_display_names, data_qualities = (
-            set(s)
-            for s in zip(
-                *[
-                    (r["LocationName"], r["CatchDisplayName"], r["dataQuality"])
-                    for r in readings
-                ]
+            timestamps, values = zip(
+                *[(r["DateTime"], float(r["Value"])) for r in readings]
             )
-        )
-        return_value[mt_id] = {
-            "location_names": location_names,
-            "catch_display_names": catch_display_names,
-            "data_qualities": data_qualities,
-        }
-    return return_value
+            payload = {
+                "measure_name": mt["DisplayName"],
+                "unique_identifier": sensor_id,
+                "readings": values,
+                "timestamps": timestamps,
+            }
+            return_value.append(("/sensor/insert-sensor-readings", payload))
+        return return_value
 
 
 def example_rothamsted_ingress() -> None:
