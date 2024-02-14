5b1f10172a64b8f339e78dee94b6ce0f4769c526
Markus Hauru <mhauru@turing.ac.uk>
2024-02-14 12:32:10 +0000

    Add docstring to backend utils.

--- dtbase/backend/utils.py
+++ dtbase/backend/utils.py
@@ -12,6 +12,10 @@ from dtbase.core.utils import connect_db
 # codebase.
 Session = scoped_session[SqlaSession] | SqlaSession
 
+# These are the global database connection objects. They start out being None, since we
+# don't want simply importing this module to create a database connection. One is
+# expected to call create_global_database_connection to set these up, and then access
+# them with global_session_maker and global_engine.
 DB_ENGINE: Optional[Engine] = None
 DB_SESSION_MAKER: Optional[sessionmaker] = None
 
@@ -19,6 +23,21 @@ DB_SESSION_MAKER: Optional[sessionmaker] = None
 def create_global_database_connection(
     connection_string: str, database_name: str, reset: bool = False
 ) -> None:
+    """Create a global database connection.
+
+    This function modifies the global variables DB_ENGINE and DB_SESSION_MAKER, and
+    returns nothing.
+
+    Args:
+        connection_string (str): The database connection string.
+        database_name (str): The name of the database to connect to.
+        reset (bool, optional): If True, reset the global database connection even if it
+        already exists. Defaults to False.
+
+    Returns:
+        None
+    """
+
     global DB_ENGINE, DB_SESSION_MAKER
     if DB_ENGINE is None or reset:
         DB_ENGINE = connect_db(connection_string, database_name)
@@ -29,6 +48,10 @@ def create_global_database_connection(
 
 
 def global_session_maker() -> sessionmaker:
+    """Get the global database session maker.
+
+    If the global session maker is not set, raise a ValueError.
+    """
     if DB_SESSION_MAKER is None:
         raise ValueError(
             "DB_SESSION_MAKER is not set. "
@@ -38,6 +61,10 @@ def global_session_maker() -> sessionmaker:
 
 
 def global_engine() -> Engine:
+    """Get the global database engine.
+
+    If the global engine is not set, raise a ValueError.
+    """
     if DB_ENGINE is None:
         raise ValueError(
             "DB_ENGINE is not set. "
@@ -47,6 +74,13 @@ def global_engine() -> Engine:
 
 
 def db_session() -> Generator[Session, None, None]:
+    """Yield a database session to the global database connection.
+
+    This function is used extensively as a FastAPI dependency by any endpoint that needs
+    to access the database.
+
+    Raises a ValueError if the global database connection is not set.
+    """
     session_maker = global_session_maker()
     with session_maker() as session:
         yield session
